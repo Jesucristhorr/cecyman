a2e023d28358163b269871c6ee3d9d99a501d903
Jesus Moreira <jesucristhorr@tuta.io>
2022-01-06 00:58:56 -0500

    feat: cecy2

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,10 +2,13 @@ import type { NextPage } from "next";
 
 const Home: NextPage = () => {
   return (
-    <div className="w-full h-screen flex items-center justify-center">
+    <div className="w-full h-screen flex flex-col gap-4 items-center justify-center">
       <p className="text-7xl text-pink-600 first-letter:underline font-semibold">
         Hola, soy Cecy y soy arquitecto :D
       </p>
+      <br />
+      <img className="w-3/5 saturate-200" src="/cosarara.png" alt="cosarara" />
+      <p className="-m-4 text-gray-500 text-xs">muy buena arquitectura :)</p>
     </div>
   );
 };
